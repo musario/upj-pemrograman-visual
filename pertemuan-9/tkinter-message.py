db5c62ea914fe8855553be3dcefb71a139eac942
Muhammad Satrio <94074146+musario@users.noreply.github.com>
2024-04-26 10:05:09 +0700

    Create tkinter-message.py

---
+++ pertemuan-9/tkinter-message.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+var = tk.StringVar()
+
+message = tk.Message(root, text="Welcome to Informatika Universitas Pembangunan Jaya")
+message.pack()
+
+root.mainloop()
